94bbae79177ec70413fad5c23dca5be48a103f6e
herr-code <herrwebj@gmail.com>
2022-04-29 12:40:37 -0500

    Sprint 1 Requerimiento 2

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,6 +1,6 @@
 /*
   Requerimiento 1: Se necesita un modelo `User` para guardar la información de un usuario de nuestra app: `id`, `username`, `name`, `bio`, `dateCreated` y `lastUpdated`. 
-
+  Requerimiento 2: Las propiedades `dateCreated` y `lastUpdated` deberán ser datos de tipo fecha que guarden el momento en que se instancie un nuevo objeto de la clase `User`.
 */
 
 const User = require('./../../app/models/User')
@@ -8,7 +8,7 @@ const User = require('./../../app/models/User')
 describe("Unit Tests for User class", () => {
   test('1) Create an user object', () => {
     //Aqui invocas el código que vas a usar en tu app
-    const user = new User(1, "carogilmar", "Carlo", "Bio", "dateCreated", "lastUpdated")
+    const user = new User(1, "carogilmar", "Carlo", "Bio")
     
     // Aqui validas los resultados de ese código
     // Esta es una comparación que va a igualar el valor dela izquierda con el valor de la derecha (valor esperado)
@@ -16,8 +16,9 @@ describe("Unit Tests for User class", () => {
     expect(user.username).toBe("carogilmar");
     expect(user.name).toBe("Carlo");
     expect(user.bio).toBe("Bio");
-    expect(user.dateCreated).toBe("dateCreated");
-    expect(user.lastUpdated).toBe("lastUpdated");
+    //Verifica que el valor no sea undefined
+    expect(user.dateCreated).not.toBeUndefined();
+    expect(user.lastUpdated).not.toBeUndefined();
   });
 
   // test('2) Use the method getInfo()', () =>{
